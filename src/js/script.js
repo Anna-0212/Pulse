593da23331806c8648abffa7935de1676447ea99
Anna <annak.sunshine@gmail.com>
2023-09-27 14:16:32 +0200

    feedback

--- src/js/script.js
+++ src/js/script.js
@@ -16,4 +16,57 @@ $(document).ready(function() {
             }
         ]
     });
+
+    //подключаем наши табы
+    // копируем jquery  и адаптируем под наш код изменяя классы на наши. этот скрипт довольно универсален и работает на большое количество табов
+
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+        $(this)
+        // тут мы настраиваем кнопки переключения "для фитнеса", "для бега" и т.д., меняется цвет
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          // тут при нажатии кнопки выше идет отбор с определенным индексом среди наших табов в каталоге
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // пишем скрипт, который будет переключать информацию непосредственно внутри каждой карточки
+// $ в JQuery - это ф-ция, которая позволяет нам получать все эл-ты по определенному селектору (классы, id, теги, комбинации)
+// напр., у нас есть 10 ссылок, к-е мы получили('.catalog-item__link')
+// и для каждой ссылки each мы будем выполнять какие-то действия
+// эта комбинация -$(this) и будет ссылаться на к-ю ссылку, которую мы в данный момент "перебираем"
+    // $('.catalog-item__link').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    // });
+
+    //теперь настраиваем, чтоб при клике на кнопку назад, все возвращалось на первую часть таба
+    // $('.catalog-item__back').each(function(i) {
+    //     $(this).on('click', function(e) {
+    //         e.preventDefault();
+    //         $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+    //         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+    //     })
+    // });
+
+    //это можно все оптимизировать чтобы меньше кода повторялось
+// теперь у нас есть ф-ция toggleSlide, которая берет ссылку, к-ю мы ей передаем ('.catalog-item__link'.catalog-item__back');)
+//и выполняет с ней операции по замене toggleClass (туда и обратно)
+
+function toggleSlide(item) {
+    $(item).each(function(i) {
+        $(this).on('click', function(e) {
+            e.preventDefault();
+            $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+            $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        })
+    });
+};
+
+toggleSlide('.catalog-item__link');
+toggleSlide('.catalog-item__back');
+
+
 });
