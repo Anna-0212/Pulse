fd59d068e1bc241aba9c81a349da78da5dbddc7f
Anna <annak.sunshine@gmail.com>
2023-09-27 23:15:03 +0200

    pageup

--- src/js/script.js
+++ src/js/script.js
@@ -168,5 +168,23 @@ $('form').submit(function(e) {
     return false;
 });
 
+//Smooth scroll and pageup
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+//плавний скролл для ссилок та pageup
+$("a[href=#up]").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+});
+
+new WOW().init();
 
 });
